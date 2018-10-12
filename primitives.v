405de11dac133bd8fb68fc5421da01d6e576e40d
tancredosouza <tjssj@cin.ufpe.br>
2018-10-12 13:44:36 -0300

    Implementada soma de pontos, mas não pude testar pois tive problemas com a forma que a biblioteca Reals soma numeros reais kkk

--- primitives.v
+++ primitives.v
@@ -33,4 +33,21 @@ Example test_coord_eq2 :
   get_y (3/5 , 0) = 0.
 Proof. simpl. reflexivity. Qed.
 
+Definition sum_pts (p q : coord R R) : (coord R R) :=
+  match p, q with
+    | (a , b) , (c , d) => (a + c, b + d)
+  end.
+  
+Notation " x p+ y " := (sum_pts x y) (at level 60, right associativity).
+
+(* Não consegui somar os valores de R.. Que biblioteca estranha :S *)
+Example test_sum1 :
+  (3/5 , 7/2) p+ (1/5 , 1) = (4/5, 9/2).
+Proof. simpl. try reflexivity. Abort.
+
+Example test_sum2 :
+  (3/5 , 7/2) p+ (1/5 , 2/2) = (4/5, 9/2).
+Proof. simpl. reflexivity. Qed.
+
+
 End Primitives.
