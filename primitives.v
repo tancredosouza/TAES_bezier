6a59f92ba38ef1c14c68160b1c02d91396e61771
tancredosouza <tjssj@cin.ufpe.br>
2018-10-12 13:16:20 -0300

    Nevermind, tive problemas sim e tive que colocar o Local Open Scope. Defini o ponto e inseri algumas notações para facilitar o uso. Criadas as funções para acessar as coordenadas x e y

--- primitives.v
+++ primitives.v
@@ -1,16 +1,36 @@
-Module Primitives.
 Require Export Reals.
 
-Inductive prod (X Y : Type) : Type :=
-  | pair : X -> Y -> prod X Y.
+Module Primitives.
+Local Open Scope R.
   
-Inductive point (x : R) (y : R) : Type :=
-  | coord : R -> R -> point x y.
+Inductive coord (X Y : Type) : Type :=
+  | point : X -> Y -> coord X Y.
+
+Arguments point {X} {Y} _ _.
 
 Notation "( x , y )" := (point x y).
+Notation " X * Y" := (prod X Y) : type_scope.
 
 Compute (point (3/5) (5/7)).
 
-Compute ((3/5),(5/7)).
+Compute ( 3/5 , 5/7 ).
+
+Definition get_x (p : coord R R) : R := 
+  match p with
+    | ( a , b ) => a
+  end.
+
+Definition get_y (p : coord R R) : R := 
+  match p with
+    | ( a , b ) => b
+  end.
+
+Example test_coord_eq1 :
+  get_x (3/5 , 7/2) = 3/5.
+Proof. simpl. reflexivity. Qed.
+
+Example test_coord_eq2 :
+  get_y (3/5 , 0) = 0.
+Proof. simpl. reflexivity. Qed.
 
 End Primitives.
