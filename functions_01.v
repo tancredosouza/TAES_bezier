ccc26f2eb0a6da723b1d829398b9462e30dbc24c
tancredosouza <tjssj@cin.ufpe.br>
2018-10-27 07:53:52 -0300

    added sum_pt_list

--- functions_01.v
+++ functions_01.v
@@ -1,16 +1,32 @@
 Add LoadPath "~/Desktop/TAES_bezier".
-Require Export primitives.
 Require Export functions.
 
 Require Export List.
 Import ListNotations.
 Require Export Coq.Arith.Factorial.
 
-Fixpoint sum_pt_list (l : list (prod Q Q)) (i j : nat): (prod Q Q) := 
-  if (beq_nat i j) 
-    then (0,0) 
-  else 
-      match l with
+Require Import PeanoNat.
+
+Fixpoint sum_pt_list (l : list (prod Q Q)) (i j count : nat): (prod Q Q) := 
+  match count with
+    | O => (0,0) 
+    | S count' =>
+        match l with
         | [] => (0, 0)
-        | h :: t => (div_q_pt h ((fact i) * (fact (j-i)))) pp+ (sum_pt_list t (S i) j)
-      end.
+        | h :: t => 
+          (
+            (
+              1 # (Pos.of_nat (fact i * fact (j-i))))
+                                qp/ 
+                                h
+          )
+           
+          pp+ 
+          
+          (
+            sum_pt_list t (S i) j count'
+          )
+        end
+  end.
+  
+  
