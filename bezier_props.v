b2688d83ae98eaff6413c247883486afa5029f64
Tancredo Jose Santos de Souza Junior <tjssj@ug3c35.windows.cin.ufpe.br>
2018-11-05 14:20:28 -0300

    provando algumas coisas sobre pontos. nao consigo fazer coq entender que 1 * a = a

--- bezier_props.v
+++ bezier_props.v
@@ -4,6 +4,7 @@ Import ListNotations.
 Check calc_bezier_polynomial.
 Check calc_point_at.
 
+
 Theorem bezier_polynomial_recursive_eq : forall (b : bezier_curve) (q : Q),
   calc_point_at b q = calc_bezier_polynomial b q.
 Proof.
@@ -11,7 +12,11 @@ Proof.
   induction b as [ | h b' ].
   - unfold calc_point_at. unfold calc_bezier_polynomial.
     simpl. reflexivity.
-  -  
-    
+  - unfold calc_bezier_polynomial. unfold calc_point_at. simpl. simpl. destruct (length b').
+    +  simpl. unfold minus_1_sgn. simpl. unfold calc_fact_div. simpl.
+       repeat (rewrite product_id). reflexivity.
+    +  
+       
+       
 
 Qed.
--- primitives.v
+++ primitives.v
@@ -11,12 +11,12 @@ Notation "( x , y )" := (point x y).
 
 Definition div_q_pt (l:Q) (r : prod Q Q) : prod Q Q := 
   match r with
-    | pair x y => (Qred (l / x), Qred (l / y))
+    | pair x y => ((Qred (l / x), Qred (l / y)))
   end.
 
 Definition mul_q_pt (l: Q) (r: prod Q Q): prod Q Q :=
   match r with
-    | pair x y => (Qred (l * x), Qred(l * y))
+    | pair x y => ((l * x), (l * y))
   end.
 
 Definition sum_pt (l: prod Q Q) (r: prod Q Q): prod Q Q :=
@@ -24,10 +24,45 @@ Definition sum_pt (l: prod Q Q) (r: prod Q Q): prod Q Q :=
   | pair xl yl, pair xr yr => (Qred(xl + xr), Qred(yl + yr))
   end.
 
-Notation "l qp* r" := (mul_q_pt l r) (at level 71, left associativity).
+Notation "l qp* r" := (mul_q_pt l r) (at level 74, left associativity).
 
-Notation "l qp/ r" := (div_q_pt l r) (at level 71, left associativity).
+Notation "l qp/ r" := (div_q_pt l r) (at level 74, left associativity).
 
-Notation "l pp+ r" := (sum_pt l r) (at level 81, left associativity).
+Notation "l pp+ r" := (sum_pt l r) (at level 76, left associativity).
+
+Definition pt_fst (p : prod Q Q) : Q :=
+  match p with
+    | pair a b => a
+  end.
+
+Definition pt_snd (p : prod Q Q) : Q :=
+  match p with
+    | pair a b => b
+  end.
+
+Definition eq_pt (p q : prod Q Q) : bool :=
+  match Qeq_bool (pt_fst p) (pt_fst q) with
+    | false => false
+    | true => 
+      match Qeq_bool (pt_snd p) (pt_snd q) with
+        | false => false
+        | true => true
+      end
+  end.
+
+Lemma Qmult_1_l' : forall (q : Q), 1 * q = q.
+Proof.
+Admitted.
+
+Compute (Qeq_bool (1 * (inject_Z 3)) (inject_Z 3)).
+
+Lemma product_id : forall (p : prod Q Q),
+  eq_pt p (1 qp* p) = true.
+Proof.
+  intros p. destruct p as [ a b ].
+  simpl. unfold point. rewrite Qmult_1_l'. rewrite Qmult_1_l'. unfold eq_pt. simpl.
+  rewrite Qeq_bool_refl. rewrite Qeq_bool_refl. reflexivity.
+Qed.
+
 
 Definition bezier_curve := list (prod Q Q).
