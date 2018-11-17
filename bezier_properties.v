11f507f98ee1bd7fa96aca1986d470429f5bdb9e
tancredosouza <tjssj@cin.ufpe.br>
2018-11-17 08:20:58 -0300

    provei simetria pra primeira ordem

--- bezier_properties.v
+++ bezier_properties.v
@@ -93,11 +93,56 @@ Proof.
   {
     rewrite HY3. rewrite HY4. apply Qeq_refl.
   }
+   
 Qed.
 
 
 Definition l2 := [(1 # 2, 1 # 4); (3 # 4, 8 # 4)].
 Compute (calc_bezier_recursive l2 (33 # 17)).
+
+
+Lemma bezier_curve_recursive_symm_fstdegree : 
+  forall (b : bezier_curve) (P0 P1 : point) (q : Q),
+    b = [P0; P1] -> 
+      calc_bezier_recursive b q == calc_bezier_recursive (rev b) (1 - q).
+Proof.
+  intros b P0 P1 q H.
+  apply (bezier_curve_fst_order_recursive b P0 P1 q) in H as H1.
+  apply (bezier_curve_fst_order_recursive_rev b P0 P1 q) in H as H2.
+  
+  destruct (calc_bezier_recursive b q) as [ x3 y3 ].
+  destruct (calc_bezier_recursive (rev b) (1 - q)) as [ x4 y4 ].
+  
+  destruct P0 as [ x0 y0 ]. destruct P1 as [ x1 y1 ].
+  assert (HX3 : Qeq x3 ((1 - q) * x0 + q * x1)).
+  {
+    unfold "==" in H1. destruct H1 as [Ha Hb].
+    simpl in Ha. apply Ha.
+  }
+  assert (HX4 : Qeq x4 ((1 - q) * x0 + q * x1)).
+  {
+    unfold "==" in H2. destruct H2 as [Ha Hb].
+    simpl in Ha. apply Ha.
+  }
+  assert (HY3 : Qeq y3 ((1 - q) * y0 + q * y1)).
+  {
+    unfold "==" in H1. destruct H1 as [Ha Hb].
+    simpl in Hb. apply Hb.
+  }
+  assert (HY4 : Qeq y4 ((1 - q) * y0 + q * y1)).
+  {
+    unfold "==" in H2. destruct H2 as [Ha Hb].
+    simpl in Hb. apply Hb.
+  }
+  
+  unfold "==". simpl. split.
+  {
+    rewrite HX3. rewrite HX4. apply Qeq_refl.
+  }
+  {
+    rewrite HY3. rewrite HY4. apply Qeq_refl.
+  }
+Qed.
 
 Theorem bezier_curve_recursive_symm_firsttry : forall (b b' : bezier_curve) (P0 P1 : point) (q : Q),
   b = [P0; P1] ++ b' -> calc_bezier_recursive b q == calc_bezier_recursive (rev b) (1 - q).
@@ -113,16 +158,8 @@ Proof.
       rewrite eq1. reflexivity.
     }
     apply (bezier_curve_fst_order_recursive (rev b) P1 P0 (1 - q)) in rev_eq1 as H1.
-    rewrite H0. rewrite H1.
-    unfold "==". split.
-    {
-      destruct P0 as [x0 y0]. destruct P1 as [x1 y1].
-      simpl. ring. 
-    }
-    {
-      destruct P0 as [x0 y0]. destruct P1 as [x1 y1].
-      simpl. ring.
-    }
+    try rewrite H0. try rewrite H1.
+    destruct (calc_bezier_recursive b q) as []
   }
 Admitted.
 
