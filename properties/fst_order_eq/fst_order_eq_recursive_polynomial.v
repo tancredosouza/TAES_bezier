670eada6d38ff393f90c9a847360974ac759e3d4
tancredosouza <tjssj@cin.ufpe.br>
2018-11-23 22:20:44 -0300

    tentando provar equivalencia

--- properties/fst_order_eq/fst_order_eq_recursive_polynomial.v
+++ properties/fst_order_eq/fst_order_eq_recursive_polynomial.v
@@ -6,18 +6,52 @@ Import auxiliary.
 Require Import QArith.
 Require Import Coq.Setoids.Setoid.
 Require Import Coq.Classes.RelationClasses.
+Require Import Coq.Classes.Morphisms.
+
+Lemma bezier_curve_recursive_polynomial_app_eq :
+  forall (b : bezier_curve) (p : point) (q : Q),
+    (calc_bezier_recursive b q) == (calc_bezier_polynomial b q) ->
+      (calc_bezier_recursive (p :: b) q) == (calc_bezier_polynomial (p :: b) q).
+Proof.
+  intros b.
+  induction b.
+  {
+    admit.
+  }
+  {
+    intros p' q' IHp.
+    unfold calc_bezier_polynomial. 
+    unfold calc_bezier_recursive.
+    induction bezier_curve_length.
+    {
+      simpl. apply eq_pt_refl.
+    }
+    {
+      simpl calc_polynomial.
+    }
+  }
+Admitted.
 
 Theorem bezier_curve_recursive_polynomial_eq_fstorder : 
-  forall (b : bezier_curve) (P0 P1 : point) (q : Q),
-    b = P0 :: [P1] -> calc_bezier_recursive b q == (calc_bezier_polynomial b q).
+  forall (b : bezier_curve) (q : Q),
+    (calc_bezier_recursive b q) == (calc_bezier_polynomial b q).
 Proof.
-  intros b P0 P1 q H.
-  rewrite H. unfold calc_bezier_recursive. unfold calc_bezier_polynomial.
-  - assert (calc_bezier_recursive b q == (((1 - q) qp* P0) pp+ (q qp* P1))).
+  intros b.
+  induction b.
+  {
+    intros q. unfold calc_bezier_recursive. unfold calc_bezier_polynomial.
+    simpl. unfold calc_fact_div. unfold minus_1_sgn. simpl.
+    unfold "==". destruct p as [ x y ]. simpl. split.
     {
-      destruct P0 as [ x0 y0 ]. destruct P1 as [ x1 y1 ].
-      rewrite H. unfold calc_bezier_recursive. simpl.
+      ring.
     }
-  
-  
+    {
+      ring.
+    }
+  }
+  {
+    intros q.
+    apply bezier_curve_recursive_polynomial_app_eq.
+    apply IHb.
+  }
 Qed.
